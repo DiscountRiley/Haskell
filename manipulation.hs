cb80f02b3b814647a67f38c5e7c7f2f34b6631d1
DiscountRiley <rrileygeorge@gmail.com>
2019-01-08 10:04:04 -0500

    a

--- manipulation.hs
+++ manipulation.hs
@@ -1,2 +1,11 @@
-manipulation.hs
+main = do print "All Good!"
+
+manipulateStr :: String -> Int -> String
 manipulateStr str x = if mod x 2 == 0 then str else reverse str
+
+boustrophedon :: [String] -> [String]
+boustrophedon strs = [manipulateStr str x | (str,x) <- zip strs [0,1..]]
+
+split20 :: String -> [String]
+split20 [] = []
+split20 str = [(take 20 str)] ++ (split20 (drop 20 str))
